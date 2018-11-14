4126796686f330ca1dea018aed621a111364e52f
GriezSwift <449106803@qq.com>
2018-11-14 23:56:47 +0800

    settings/allowed hosts

--- InsMaster/settings.py
+++ InsMaster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@zu!)8u(u+#ts((d$u#o^r0$7#gt&emn8xt0!f==w$qifxcvjt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['140.143.208.38']
 
 
 # Application definition
